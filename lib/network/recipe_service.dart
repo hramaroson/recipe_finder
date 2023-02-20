3fbeefcaca42710beae780f9ecd08d3984ade286
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-02-20 23:59:35 +0300

    Add model_converter.dart

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,23 +1,15 @@
-import 'dart:developer';
-import 'package:http/http.dart';
+import 'package:chopper/chopper.dart';
 
+import 'model_response.dart';
+import 'recipe_model.dart';
+import 'model_converter.dart';
 import 'api_credentials.dart';
 
-class RecipeService{
-  Future getData(String url) async{
-     final response = await get(Uri.parse(url));
-     if(response.statusCode == 200){
-        return response.body;
-     }
-     else {
-      log(response.body);
-     }
-  }
-
-  Future<dynamic> getRecipes(String query, int from, int to) async{
-    final recipeData = await getData('$apiUrl?'
-      'app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
-
-    return recipeData;
-  }
+@ChopperApi()
+abstract class RecipeService extends ChopperService{
+  @Get(path: 'search')
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+    @Query('q') String query, @Query('from') int from, 
+    @Query('to') int to);
 }
+
