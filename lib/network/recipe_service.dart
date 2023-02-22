c163df6caf9c0f7fc7117c22947e4c1a05498d3a
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-02-22 19:56:10 +0300

    Implement RecipeService create()

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -5,11 +5,41 @@ import 'recipe_model.dart';
 import 'model_converter.dart';
 import 'api_credentials.dart';
 
+part 'recipe_service.chopper.dart';
+
+// Use the following command in terminal to generate recipe_service.chopper.dart
+// flutter pub run build_runner build --delete-conflicting-outputs
+// To run every time the file changes, use:
+// flutter pub run build_runner watch
+
+Request _addQuery(Request req){
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+  
+  return req.copyWith(parameters: params);
+}
+
 @ChopperApi()
 abstract class RecipeService extends ChopperService{
   @Get(path: 'search')
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
     @Query('q') String query, @Query('from') int from, 
     @Query('to') int to);
+  
+  static RecipeService create(){
+    final client = ChopperClient(
+      baseUrl: Uri(path: apiUrl),
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [
+        _$RecipeService()
+      ]
+    );
+
+    return _$RecipeService(client);
+  }
 }
+
 
