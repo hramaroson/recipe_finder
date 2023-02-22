21fbf42674bff0f807020c20ea8e38e83f638954
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-02-23 00:43:49 +0300

    _setupLogging()

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'package:logging/logging.dart';
 
 import 'ui/main_screen.dart';
 
 Future<void> main() async {
+  _setupLogging();
+
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+}
+
+void _setupLogging(){
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(
+    (rec) { 
+      log('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
 }
 
 class MyApp extends StatelessWidget {
