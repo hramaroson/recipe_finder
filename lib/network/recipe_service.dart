50e5b80c29f886f59e21c29c9209686a2f6f38a7
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2024-05-13 23:19:52 +0300

    Fix RecipeService Class

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -3,6 +3,7 @@ import 'package:chopper/chopper.dart';
 import 'model_response.dart';
 import 'recipe_model.dart';
 import 'model_converter.dart';
+import 'service_interface.dart';
 import 'api_credentials.dart';
 
 part 'recipe_service.chopper.dart';
@@ -12,34 +13,35 @@ part 'recipe_service.chopper.dart';
 // To run every time the file changes, use:
 // flutter pub run build_runner watch
 
-Request _addQuery(Request req){
-  final params = Map<String, dynamic>.from(req.parameters);
-  params['app_id'] = apiId;
-  params['app_key'] = apiKey;
-  
-  return req.copyWith(parameters: params);
-}
-
 @ChopperApi()
-abstract class RecipeService extends ChopperService{
+abstract class RecipeService extends ChopperService
+    implements ServiceInterface {
+  @override
   @Get(path: 'search')
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
-    @Query('q') String query, @Query('from') int from, 
-    @Query('to') int to);
-  
-  static RecipeService create(){
+    @Query('q') String query,
+    @Query('from') int from,
+    @Query('to') int to,
+  );
+
+  static RecipeService create() {
     final client = ChopperClient(
       baseUrl: Uri.parse(apiUrl),
       interceptors: [_addQuery, HttpLoggingInterceptor()],
       converter: ModelConverter(),
       errorConverter: const JsonConverter(),
       services: [
-        _$RecipeService()
-      ]
+        _$RecipeService(),
+      ],
     );
-
     return _$RecipeService(client);
   }
 }
 
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
 
+  return req.copyWith(parameters: params);
+}
